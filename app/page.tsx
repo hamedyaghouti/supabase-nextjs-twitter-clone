30074ca6fbe0415622148a0b10342b898314fda8
h.yaghooti <hamedyaghooti1992@gmail.com>
2023-11-13 18:46:42 +0330

    using supabase UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,42 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
 
-import AuthButtonServer from "./auth-button-server";
+// import AuthButtonServer from "./auth-button-server";
 
-export const dynamic = "force-dynamic";
-export const runtime = "edge";
+// export const dynamic = "force-dynamic";
+// export const runtime = "edge";
 
-export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data: tweets } = await supabase.from("tweets").select();
+// export default async function Home() {
+//   const supabase = createServerComponentClient({ cookies });
+//   const { data: tweets } = await supabase.from("tweets").select();
 
-  return (
-    <>
-      <AuthButtonServer />
-      <pre>{JSON.stringify(tweets, null, 2)}</pre>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <AuthButtonServer />
+//       <pre>{JSON.stringify(tweets, null, 2)}</pre>
+//     </>
+//   );
+// }
+"use client";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from "@supabase/auth-ui-shared";
+
+// const supabase = createClient('<INSERT PROJECT URL>', '<INSERT PROJECT ANON API KEY>')
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+);
+
+const App = () => (
+  <Auth
+    supabaseClient={supabase}
+    appearance={{ theme: ThemeSupa }}
+    providers={["github", "figma"]}
+  />
+);
+
+export default App;
